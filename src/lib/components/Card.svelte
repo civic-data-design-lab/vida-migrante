0ee43120ec38272863e5d0d240d78a9e4e5c769a
Enrique <enriquec@mit.edu>
2023-02-21 17:37:53 -0500

    Created initial card and decision logic

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -4,5 +4,17 @@
 </script>
 
 <article>
-  <h1>{card.description}</h1>
+  <h1>{card.category}</h1>
+  <img src={'/favicon.png'} alt="" />
+  <p>{card.description}</p>
+  <!-- TODO: Show resources required as an icon, as in Figma -->
+  <p>Resources Required: {card.resourcesRequired}</p>
 </article>
+
+<style>
+  article {
+    border: 1px solid black;
+    border-radius: 1rem;
+    padding: 1rem;
+  }
+</style>
