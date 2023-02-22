b8f64262fcec33713da98629ce70cf631b77dc5f
Enrique <enriquec@mit.edu>
2023-02-21 23:08:20 -0500

    Draw card page semi-finalized

--- src/routes/game/DrawCardPage.svelte
+++ src/routes/game/DrawCardPage.svelte
@@ -2,6 +2,7 @@
   import { GameData } from '$gameData';
   import { NUM_TO_ORDINAL_ARR } from '$lib/utils/types';
   import cardBack from '$images/card-back.png';
+  import touchIcon from '$lib/assets/images/touch-app.svg';
 
   // FIXME: Fix once we are keeping track of round number
   $: round = $GameData.round;
@@ -10,14 +11,15 @@
 
 <h1>Round {round + 1}</h1>
 <!-- Game Data store will automatically draw a card -->
-<button on:click={() => GameData.advanceGameState()}>
+<button id="draw-card" on:click={() => GameData.advanceGameState()}>
   <div class="tap-indicator">
-    <h6>Tap to draw a card</h6>
+    <img src={touchIcon} alt="" />
+    <p for="draw-card">Tap to draw a card</p>
   </div>
   <div class="fade-in-out">
-    <img src={cardBack} alt="Card back" />
-    <img src={cardBack} alt="Card back" />
-    <img src={cardBack} alt="Card back" />
+    <img class="card-back" src={cardBack} alt="Card back" />
+    <img class="card-back" src={cardBack} alt="Card back" />
+    <img class="card-back" src={cardBack} alt="Card back" />
   </div>
 </button>
 
@@ -44,11 +46,13 @@
     z-index: 1;
     inset: 0;
     height: 100%;
-    display: grid;
-    place-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
 
-  img {
+  .card-back {
     position: absolute;
     object-fit: cover;
     width: 100%;
@@ -57,10 +61,10 @@
     filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
   }
 
-  img:nth-child(1) {
+  .card-back:nth-child(1) {
     transform: rotate(5deg);
   }
-  img:nth-child(2) {
+  .card-back:nth-child(2) {
     transform: rotate(-5deg);
   }
 </style>
