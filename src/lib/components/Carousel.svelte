73ccef771f0a39c58c41f9d889b45fdf79632927
Enrique <enriquec@mit.edu>
2023-02-22 23:45:22 -0500

    [temp] Moved dynamically-rendered images to static dir

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -14,7 +14,7 @@
           <a href="#carousel-slide{i + 1}" class="carousel-next">Go to next slide</a>
         {/if}
         <div class="carousel-content">
-          <img src="/src/lib/assets/images/{migrant.name}.png" alt="Manuel" />
+          <img src={`/images/migrants/${migrant.name}.png`} alt="Manuel" />
           <h3>{migrant.name}</h3>
           <span>{migrant.age}, {migrant.maritalStatus}</span>
           {@html migrant.introText}
