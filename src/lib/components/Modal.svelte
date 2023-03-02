1462c886f2af7a32e254279e79efad33235a7456
Xyresic <ericjmlam@gmail.com>
2023-03-01 21:23:13 -0500

    Create modal component

---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let showModal;
+  let dialog;
+  $: if (dialog && showModal) dialog.showModal();
+</script>
+
+<dialog
+  bind:this={dialog}
+  on:close={() => {
+    showModal = false;
+  }}
+  on:click|self={() => dialog.close()}
+>
+  <button on:click={() => dialog.close()}> X </button>
+  <slot name="body" />
+</dialog>
+
+<style>
+  dialog {
+    max-width: 90vw;
+    max-height: 90vh;
+  }
+
+  dialog::backdrop {
+    background: rgba(0, 0, 0, 0.7);
+  }
+</style>
