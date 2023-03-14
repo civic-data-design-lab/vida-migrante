b8a2cd3ee5720531062b3af40fc689447634b753
Xyresic <ericjmlam@gmail.com>
2023-03-14 07:31:20 -0400

    Increase handle hitbox

--- src/lib/components/Drawer.svelte
+++ src/lib/components/Drawer.svelte
@@ -3,8 +3,10 @@
 
   let dragging = false;
   let windowHeight, topThreshold, botThreshold, drawerTop;
-  let previousTouch, drawer, cancelClick = false;
-  let transition = '';
+  let handle,
+    cancelClick = false;
+  let prevTouch,
+    transition = '';
 
   onMount(() => {
     const vh = windowHeight / 100;
@@ -17,9 +19,8 @@
     drawerTop = Math.min(Math.max(newVal, topThreshold), botThreshold);
   }
 
-  function onDragStart(e) {
+  function onMouseDown() {
     dragging = true;
-    if (e.type === 'touchstart') previousTouch = e.touches[0];
     transition = '';
   }
 
@@ -30,52 +31,69 @@
     }
   }
 
+  function onMouseUp(e) {
+    dragging = false;
+    if (!e.target.id.startsWith('drawer-handle')) cancelClick = false;
+  }
+
+  function onTouchStart(e) {
+    dragging = true;
+    transition = '';
+    prevTouch = e.touches[0];
+  }
+
   function onTouchMove(e) {
     const touch = e.touches[0];
     if (dragging) {
-      const movementY = touch.pageY - previousTouch.pageY;
+      const movementY = touch.clientY - prevTouch.clientY;
       boundDrag(drawerTop + movementY);
       cancelClick = true;
     }
-    previousTouch = touch;
+    prevTouch = touch;
   }
 
-  function onDragEnd() {
+  function onTouchEnd() {
     dragging = false;
-    previousTouch = null;
-    if (cancelClick) {
-      drawer.addEventListener('click', captureClick, {capture: true, once: true});
+    if (prevTouch !== null) {
+      const rect = handle.getBoundingClientRect();
+      const x = prevTouch.clientX,
+        y = prevTouch.clientY;
+      if (x < rect.left || y < rect.top || x > rect.right || y < rect.bottom) {
+        cancelClick = false;
+      }
+      prevTouch = null;
     }
-    cancelClick = false;
-  }
-
-  function captureClick(e) {
-    e.stopPropagation();
   }
 
   function toggleDrawer() {
-    transition = 'top 1s ease';
-    const tolerance = (botThreshold - topThreshold) / 5;
-    if (botThreshold - drawerTop < tolerance) drawerTop = topThreshold;
-    else drawerTop = botThreshold;
+    if (cancelClick) cancelClick = false;
+    else {
+      transition = 'top 1s ease';
+      const tolerance = (botThreshold - topThreshold) / 5;
+      if (botThreshold - drawerTop < tolerance) drawerTop = topThreshold;
+      else drawerTop = botThreshold;
+    }
   }
 </script>
 
-<div id="floating" style="top: {drawerTop}px; transition: {transition}" bind:this={drawer}>
+<div id="floating" style="top: {drawerTop}px; transition: {transition}">
   <div
-    id="drawer-handle"
-    on:touchstart={onDragStart}
-    on:mousedown={onDragStart}
+    id="drawer-handle-hitbox"
+    on:touchstart={onTouchStart}
+    on:mousedown={onMouseDown}
     on:click={toggleDrawer}
-  />
+    bind:this={handle}
+  >
+    <div id="drawer-handle" />
+  </div>
   <slot name="body" />
 </div>
 <svelte:window
   bind:innerHeight={windowHeight}
   on:touchmove={onTouchMove}
-  on:touchend={onDragEnd}
+  on:touchend={onTouchEnd}
   on:mousemove={onMouseMove}
-  on:mouseup={onDragEnd}
+  on:mouseup={onMouseUp}
 />
 
 <style>
@@ -93,12 +111,20 @@
     background: white;
   }
 
+  #drawer-handle-hitbox {
+    height: 20px;
+    width: 75px;
+    margin-block: calc(2.5vh - 8px);
+    cursor: ns-resize;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
   #drawer-handle {
     background: rgba(150, 150, 150, 0.8);
     height: 8px;
     width: 50px;
     border-radius: 4px;
-    margin-block: calc(2.5vh - 8px);
-    cursor: ns-resize;
   }
 </style>
