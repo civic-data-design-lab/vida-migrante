d406ee1e416466acd8935b1674165a151f1c2412
Xyresic <ericjmlam@gmail.com>
2023-04-05 02:33:36 -0400

    Add summary to game end page

--- src/routes/game/_pages/GameEndPage.svelte
+++ src/routes/game/_pages/GameEndPage.svelte
@@ -7,54 +7,32 @@
   $: job = jobs[$GameData.jobId];
   let income = $GameData.resources?.income.salary + $GameData.resources?.income.assistance;
 
-  let showModal = false;
-  let success = true;
-  function toggle() {
-    success = !success;
-  }
+  $: success = income > job.income;
 </script>
 
 <h1>
-  Your household is {#if success}doing better!{:else}still struggling{/if}
+  You and your family are {#if success}doing better!{:else}still struggling{/if}
 </h1>
-<Modal bind:showModal={showModal}>
-  <div id="modal-body" slot="body">
-    <h1>
-      You and your family are {#if success}doing better!{:else}still struggling{/if}
-    </h1>
-    <p>
-      Your income went from <b>${job.income}</b> to <b class="accent-green">${income}</b>. Your
-      household income is still below
-      <b class="accent-red">$793, the National Average in Ecuador</b>{#if success}.{:else},
-      and it's still not enough to provide for your family's basic needs,
-      including the <b>Family Baskets</b> of food.{/if}
-    </p>
-    {#if success}
-      <p>
-        Your income is still not enough to buy a <b class="accent-yellow">Basic Family Basket</b> of
-        food, but you can afford a <b class="accent-blue">Vital Family Basket</b>.
-      </p>
-    {/if}
-    <p>
-      You work {job.hours} hours every week.
-    </p>
-    <button
-      id="modal-button"
-      class="button"
-      on:click={() => {
-        showModal = false;
-      }}>OK</button
-    >
-  </div>
-</Modal>
+<div id="body">
+<p>
+  Your income went from <b>${job.income}</b> to <b class="accent-green">${income}</b>. Your
+  household income is still below
+  <b class="accent-red">$793, the National Average in Ecuador</b>{#if success}.{:else},
+  and it's still not enough to provide for your family's basic needs,
+  including the <b>Family Baskets</b> of food.{/if}
+</p>
+{#if success}
+  <p>
+    Your income is still not enough to buy a <b class="accent-yellow">Basic Family Basket</b> of
+    food, but you can afford a <b class="accent-blue">Vital Family Basket</b>.
+  </p>
+{/if}
+<p>
+  You work {job.hours} hours every week.
+</p>
+</div>
 
 <div id="footer">
-  <button on:click={toggle}>Toggle Success</button>
-  <button
-    on:click={() => {
-      showModal = true;
-    }}>Show Modal</button
-  >
   <button class="button" on:click={GameData.advanceGameState}> Try Again </button>
   <a href="/about">About</a>
   <a href="/policy">Policy Recommendations</a>
@@ -63,10 +41,16 @@
 <style>
   h1 {
     text-align: center;
-    font-size: min(43pt, 12vw);
+    font-size: min(30pt, 12vw);
     padding-left: 2.5vw;
     padding-right: 2.5vw;
   }
+
+  #body {
+    padding-left: 2.5vw;
+    padding-right: 2.5vw;
+  }
+
   #footer {
     height: 15vh;
     width: 100%;
@@ -81,18 +65,5 @@
     width: 50%;
     border-radius: 3vh;
     font-size: 18pt;
-  }
-
-  #modal-body {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-evenly;
-  }
-
-  #modal-button {
-    height: 5vh;
-    width: 50%;
-    border-radius: 2.5vh;
   }
 </style>
