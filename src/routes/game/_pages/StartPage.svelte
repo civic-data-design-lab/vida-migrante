bfb3c25cda03d732c3d817a2d2d1734c6db5b9f2
Enrique <enriquec@mit.edu>
2023-04-07 02:46:12 -0400

    Made start page language responsive

--- src/routes/game/_pages/StartPage.svelte
+++ src/routes/game/_pages/StartPage.svelte
@@ -1,8 +1,18 @@
 <script>
   import Timed from '$lib/components/Timed.svelte';
   import { GameData } from '$lib/stores/gameData';
+  import { page } from '$app/stores';
+  import { Languages } from '$lib/utils/types';
+
+  const language = $page.data.language;
 </script>
 
 <Timed waitTimeMs={2000} callback={GameData.advanceGameState}>
-  <h1>Start Game</h1>
+  <h1>
+    {#if language === Languages.ENGLISH}
+      Entering Ecuador...
+    {:else}
+      Entrando a Ecuador...
+    {/if}
+  </h1>
 </Timed>
